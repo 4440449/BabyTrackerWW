60d7f470ba1dc5fe5655d4f11d5f713d056cc3e3
Max Terpugov <maksim_terpugov@mail.ru>
2021-09-29 18:41:44 +0300

    Initial Commit

---
+++ BabyTrackerWW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BabyTrackerWW
+//
+//  Created by Max on 29.09.2021.
+//  Copyright © 2021 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
